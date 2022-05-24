f12cb03d98caac069f2106b0da19c0c72f4ab2dc
wasiushisir <wasiuahmed410@gmail.com>
2022-05-24 20:23:45 +0600

    admin

--- src/components/Dashboard/Alluser.js
+++ src/components/Dashboard/Alluser.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
 
 const Alluser = () => {
 
-    const { data:users, isLoading, } = useQuery('users', () =>
-    fetch('http://localhost:5000/user').then(res =>
+    const { data:users, isLoading, refetch} = useQuery('users', () =>
+    fetch('http://localhost:5000/user',{
+        method:'GET',
+        headers:{
+            'authorization':`Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res =>
       res.json()
     )
   )
@@ -14,10 +20,59 @@ const Alluser = () => {
   }
 
 
+  const makeadmin=(email)=>{
+
+    fetch(`http://localhost:5000/user/admin/${email}`,{
+        method:'PUT',
+        headers:{
+            'authorization':`Bearer ${localStorage.getItem('accessToken')}`
+        }
+    })
+
+    .then(response=>response.json())
+    .then(data=>{console.log(data)
+        refetch();
+        toast.success('Make an admin successfully')
+    
+    })
+
+  }
+
+
 
     return (
         <div>
-            <h2>all user:{users?.length}</h2>
+            <div class="overflow-x-auto">
+  <table class="table w-full">
+   
+    <thead>
+      <tr>
+        <th></th>
+        <th>Email</th>
+        <th>Button</th>
+        <th>Button</th>
+      </tr>
+    </thead>
+    <tbody>
+
+        {
+            users.map((user,index)=> <tr>
+                <th>{index+1}</th>
+                <td>{user.email}</td>
+                <td>{user.role!=='admin'&&<button onClick={()=>makeadmin(user?.email)} className='btn btn-xs btn-success'>Make Admin</button>}</td>
+                <td>Blue</td>
+              </tr>)
+        }
+     
+     
+    
+     
+     
+      
+    </tbody>
+  </table>
+</div>
+           
             
         </div>
     );
